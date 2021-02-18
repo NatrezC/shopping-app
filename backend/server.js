c4007d9a67811ff4172733ee49addb7a1c8e09ec
NatrezC <cnatrez@gmail.com>
2021-02-18 15:08:12 -0600

    able to run backend and frontend at the same time

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
 
-const PORT =  process.env.PORT ||4000
+const PORT =  process.env.PORT ||5000
 
 app.listen(PORT, () => {
     console.log(`Backend server is running on port ${PORT}`)
