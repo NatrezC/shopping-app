46e24ee8b825ed929a7321abd2090dc0354a8d97
NatrezC <cnatrez@gmail.com>
2021-02-19 10:34:03 -0600

    created my routes for screen to work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,14 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+//Screens
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
-    <div className="app">
+    <Router>
       {/* Navbar */}
       {/* SideDrawer */}
       {/* BackDrop */}
@@ -16,8 +20,8 @@ function App() {
           <Route exact path="/cart" component={CartScreen}/>
         </Switch>
       </main>
-      
-    </div>
+    </Router>
+
   );
 }
 
