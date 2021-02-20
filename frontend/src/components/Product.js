7fca29103607fac806a20f466f7780a0fac7adcb
NatrezC <cnatrez@gmail.com>
2021-02-19 20:54:43 -0600

    set up the cart items

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Product = () => {
     return (
         <div className="product">
-            <img src="https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80" alt="item" />
-            <div className="product__info">
+            
+            <div className="product__info"><img src="https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80" alt="item" />
                 <p className="info__name"></p>
                 <p className="info__description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,23 @@
 import './CartScreen.css'
+import CartItem from '../components/CartItem';
 
 const CartScreen = () => {
     return (
-        <div className="cartScreen">
-            Cart Screen
+        <div className="cartscreen">
+            <div className="cartscreen_left">
+                <h2>Shopping Cart</h2>
+
+                <CartItem/>
+            </div>
+            <div className="cartscreen_right">
+                <div className="cartscreen__info">
+                    <p>Subtotal (0) items</p>
+                    <p> $500</p>
+                </div>
+                <div>
+                    <button>Proceed To Checkout</button>
+                </div>
+            </div>
         </div>
     )
 }
