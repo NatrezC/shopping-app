af8cbe12bd30f1dbb2528fa95ba9ff8c822acdf5
NatrezC <cnatrez@gmail.com>
2021-02-19 23:52:55 -0600

    encountering a bug with redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,31 @@
 import './HomeScreen.css';
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import Product from '../components/Product';
 
+//Actions
+import {getProducts as listProducts} from '../redux/actions/productActions'
+
 const HomeScreen = () => {
+
+    const dispatch = useDispatch()
+    
+    const getProducts = useSelector(state => state.getProducts);
+    const { products, loading, error } = getProducts; // before mapping check for loading or errors
+
+    useEffect(() => {
+        dispatch(listProducts())
+    },[dispatch])
+
+
     return (
         <div className="homeScreen">
             <h2 className="homescreen__title">My Products</h2>
 
             <div className="homescreen__products">
-                <Product />
+                {loading ? <h2>Loading...</h2> : error ? <h2>{error}</h2> : products.map(product => (
+                    <Product />
+                ))}
             </div>
             Home Screen
         </div>
